fc09a6ef1963944faff99e380944d0517c0d29f2
4YU5H25 <ayush4yush@gmail.com>
2024-03-02 02:07:27 +0530

    2-3-24

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:chat/pages/chat_page.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,10 +9,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  Future<void> audio_permission() async {
+    await Permission.audio;
+    await Permission.manageExternalStorage;
+    await Permission.microphone;
+    await Permission.accessMediaLocation;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mental Wellness',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,105 +1,186 @@
-import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
-import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chatgpt_tutorial_yt/consts.dart';
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:permission_handler/permission_handler.dart';
+// import 'package:tflite_audio/tflite_audio.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat with TFLite Model',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatPage(),
+    );
+  }
+}
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({super.key});
-
   @override
-  State<ChatPage> createState() => _ChatPageState();
+  _ChatPageState createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
-  final _openAI = OpenAI.instance.build(
-    token: OPENAI_API_KEY,
-    baseOption: HttpSetup(
-      receiveTimeout: const Duration(
-        seconds: 5,
-      ),
-    ),
-    enableLog: true,
+  TextEditingController message = TextEditingController();
+
+  String _text = '';
+  static ChatUser user = ChatUser(
+    id: '1',
+    firstName: 'Charles',
+    lastName: 'Leclerc',
+  );
+  static ChatUser model = ChatUser(
+    id: '2',
+    firstName: 'AI',
+    lastName: 'Helper',
   );
 
-  final ChatUser _currentUser =
-      ChatUser(id: '1', firstName: 'Hussain', lastName: 'Mustafa');
+  List<ChatMessage> messages = <ChatMessage>[
+    ChatMessage(
+      text: 'Hey!',
+      user: user,
+      createdAt: DateTime.now(),
+    ),
+  ];
 
-  final ChatUser _gptChatUser =
-      ChatUser(id: '2', firstName: 'Chat', lastName: 'GPT');
+  @override
+  void initState() {
+    super.initState();
+    audio_permission();
+  }
 
-  List<ChatMessage> _messages = <ChatMessage>[];
-  List<ChatUser> _typingUsers = <ChatUser>[];
+  Future<void> audio_permission() async {
+    await Permission.audio;
+    await Permission.manageExternalStorage;
+    await Permission.microphone;
+    await Permission.accessMediaLocation;
+  }
+
+  void _sendMessage(String text, bool isAudio) async {
+    if (text.trim().isNotEmpty || !isAudio) {
+      // Pass input to TFLite model
+      // var output = await TfliteAudio. runModelOnText(
+      //   text: text,
+      // );
+      // Do something with output
+      // print(output);
+
+      // Add message to chat UI
+      setState(() {
+        messages.add(ChatMessage(
+          text: text,
+          user: user,
+          createdAt: DateTime.now(),
+        ));
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(
-          0,
-          166,
-          126,
-          1,
-        ),
-        title: const Text(
-          'GPT Chat',
-          style: TextStyle(
-            color: Colors.white,
-          ),
+        title: const Text('Chat with TFLite Model'),
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.blue, Colors.deepPurple])),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(8)),
+                child: DashChat(
+                  onSend: (ChatMessage message) {
+                    _sendMessage(message.text, false);
+                  },
+                  messages: messages,
+                  inputOptions: InputOptions(
+                    textController: message,
+                    inputDecoration: InputDecoration(
+                        border: InputBorder.none, // Assuming you want no border
+
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32),
+                            borderSide:
+                                const BorderSide(color: Colors.transparent))),
+                    sendOnEnter: true,
+                    alwaysShowSend: true,
+                    sendButtonBuilder: (send) {
+                      return Container(
+                        decoration: const BoxDecoration(
+                          color:
+                              Colors.transparent, // Set your desired color here
+                          shape:
+                              BoxShape.circle, // Or any other shape you prefer
+                        ),
+                        child: Row(
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.mic),
+                              splashColor: Colors.grey,
+                              style: IconButton.styleFrom(
+                                foregroundColor: Colors.white,
+                              ),
+                              onPressed: () {
+                                // Add your logic to start recording audio here
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.send_outlined),
+                              color:
+                                  Colors.white, // Adjust icon color if needed
+                              onPressed: () => _sendMessage(message.text,
+                                  false), // Replace with your send logic
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                  typingUsers: [model],
+                  currentUser: user,
+                  messageOptions: MessageOptions(
+                    messageDecorationBuilder:
+                        (message, previousMessage, nextMessage) {
+                      if (message.user == user) {
+                        return BoxDecoration(
+                            gradient: const LinearGradient(
+                                colors: [Colors.pink, Colors.purple]),
+                            borderRadius: BorderRadius.circular(20));
+                      } else {
+                        return BoxDecoration(
+                          // Default style for received messages
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-      body: DashChat(
-          currentUser: _currentUser,
-          typingUsers: _typingUsers,
-          messageOptions: const MessageOptions(
-            currentUserContainerColor: Colors.black,
-            containerColor: Color.fromRGBO(
-              0,
-              166,
-              126,
-              1,
-            ),
-            textColor: Colors.white,
-          ),
-          onSend: (ChatMessage m) {
-            getChatResponse(m);
-          },
-          messages: _messages),
     );
   }
 
-  Future<void> getChatResponse(ChatMessage m) async {
-    setState(() {
-      _messages.insert(0, m);
-      _typingUsers.add(_gptChatUser);
-    });
-    List<Messages> _messagesHistory = _messages.reversed.map((m) {
-      if (m.user == _currentUser) {
-        return Messages(role: Role.user, content: m.text);
-      } else {
-        return Messages(role: Role.assistant, content: m.text);
-      }
-    }).toList();
-    final request = ChatCompleteText(
-      model: GptTurbo0301ChatModel(),
-      messages: _messagesHistory,
-      maxToken: 200,
-    );
-    final response = await _openAI.onChatCompletion(request: request);
-    for (var element in response!.choices) {
-      if (element.message != null) {
-        setState(() {
-          _messages.insert(
-            0,
-            ChatMessage(
-                user: _gptChatUser,
-                createdAt: DateTime.now(),
-                text: element.message!.content),
-          );
-        });
-      }
-    }
-    setState(() {
-      _typingUsers.remove(_gptChatUser);
-    });
+  @override
+  void dispose() {
+    // TfliteAudio.stopAudioRecognition();
+    super.dispose();
   }
 }
